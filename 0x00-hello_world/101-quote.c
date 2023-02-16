02afe337764027da973ec569eb1be3d690dd4624
otaawoode <otaawoode@gmail.com>
2023-02-16 12:17:40 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <unistd.h>
-/**                                                   
-* main - prints a string and standard error
- * Return: 1                                          
-*/
-int main(void)                                       
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* *main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
-}
-
+	write(2, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
