a2f945a55d38fccf6c75d38649c73c219f899d1a
otaawoode <otaawoode@gmail.com>
2023-02-16 07:39:35 -0800

    x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
  #include <unistd.h>
-
  /**
-  * main - prints exactly "and that piece of art is useful"
-  * - Dora Korpar, 2015-10-19",
-  * followed by a new line, to the standard error.
-  * Return: Always 0 (Success)
+  * main - A c program that prints a line to the standard error
+  * followed by a new line, to the standard error
+  * Return 1 (Success)
   */
  int main(void)
  {
- 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
- 	return (1);
+ 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+ return (1);
  }
