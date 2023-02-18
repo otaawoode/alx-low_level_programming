02f0135e0a07f24a9409c8de8d137a1b5f5afbca
otaawoode <otaawoode@gmail.com>
2023-02-18 05:45:09 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints a string and standard error
- * Return: 1
+ * main - C program that prints exactly
+ * Return 1(success)
  */
-int main(void)                
-{     
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
