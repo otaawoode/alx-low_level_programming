b1b5f8430c556e5e3b31fa976a325796fe5cc0f3
otaawoode <otaawoode@gmail.com>
2023-03-13 08:01:45 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,24 +14,24 @@ char *create_array(unsigned int size, char c)
 
 {
 
-        char *str;
+	char *str;
 
-        unsigned int i;
-
-
-
-        str = malloc(sizeof(char) * size);
-
-        if (size == 0 || str == NULL)
-
-                return (NULL);
+	unsigned int i;
 
 
-        for (i = 0; i < size; i++)
 
-                str[i] = c;
+	str = malloc(sizeof(char) * size);
 
-        return (str);
+	if (size == 0 || str == NULL)
+
+		return (NULL);
+
+
+	for (i = 0; i < size; i++)
+
+		str[i] = c;
+
+	return (str);
 
 }
 
