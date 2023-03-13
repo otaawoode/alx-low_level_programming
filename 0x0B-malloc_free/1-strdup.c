0298ce7ea705fb38144c07c11321c87a4daa82b5
otaawoode <otaawoode@gmail.com>
2023-03-13 08:14:07 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,34 +11,26 @@ char *_strdup(char *str)
 
 {
 
-        char *aaa;
+	char *aaa;
 
-        int i, r = 0;
+	int i, r = 0;
 
+	if (str == NULL)
 
+		return (NULL);
 
-        if (str == NULL)
+	i = 0;
 
-                return (NULL);
+	while (str[i] != '\0')
+		i++;
+	aaa = malloc(sizeof(char) * (i + 1));
 
-        i = 0;
+	if (aaa == NULL)
+		return (NULL);
 
-        while (str[i] != '\0')
+	for (r = 0; str[r]; r++)
 
-                i++;
+		aaa[r] = str[r];
 
-        aaa = malloc(sizeof(char) * (i + 1));
-
-        if (aaa == NULL)
-
-                return (NULL);
-
-
-
-        for (r = 0; str[r]; r++)
-
-                aaa[r] = str[r];
-
-
-        return (aaa);
+	return (aaa);
 }
